d5860f8494de97c6596acf19ad9a2c36b91bce0a
hmhmhm0523 <mhuang6@ncsu.edu>
2021-01-06 14:46:11 -0500

    Added drag and drop to the policy items.

--- src/MainPanel/MainPanel.jsx
+++ src/MainPanel/MainPanel.jsx
@@ -8,13 +8,13 @@ import SearchBox from '../SearchBox/SearchBox';
 class MainPanel extends PureComponent {
   constructor(props) {
     super(props);
-    this.handleToggleExpand= this.handleToggleExpand.bind(this);
+    this.handleToggleExpand = this.handleToggleExpand.bind(this);
     this.state = {
       hasError: false,
     };
   }
 
-  handleToggleExpand (event) {
+  handleToggleExpand(event) {
     let button = event.target;
     let content = event.target.parentNode.nextSibling;
     button.classList.toggle("Collapsed");
@@ -27,8 +27,56 @@ class MainPanel extends PureComponent {
     console.log('MainPanel will mount');
   }
 
+  getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.PolicyItem:not(.Dragging)')];
+
+    return draggableElements.reduce((closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child }
+      }
+      else {
+        return closest;
+      }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+  }
+
   componentDidMount = () => {
     console.log('MainPanel mounted');
+
+    const draggables = document.querySelectorAll('.PolicyItem');
+    const containers = document.querySelectorAll('.DropZone');
+    draggables.forEach(draggable => {
+      draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('Dragging');
+        containers.forEach(container => {
+          container.classList.add('Hilight');
+        })
+      })
+
+      draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('Dragging');
+        containers.forEach(container => {
+          container.classList.remove('Hilight');
+        })
+
+      })
+    });
+
+    containers.forEach(container => {
+      container.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        const afterElement = this.getDragAfterElement(container, e.clientY);
+        const draggable = document.querySelector('.Dragging');
+        if (afterElement == null) {
+          container.appendChild(draggable);
+        } else {
+          container.insertBefore(draggable, afterElement);
+        }
+      })
+    })
   }
 
   componentWillReceiveProps = (nextProps) => {
@@ -87,7 +135,7 @@ class MainPanel extends PureComponent {
               <div className='TileHeader'>Package Builder</div>
               <div className='TileContent'>
                 <div className='DropZone'>
-                  Drag Policies Building Blocks Here
+                  <p>Drag Policies Building Blocks Here</p>
                 </div>
               </div>
             </div>
@@ -109,21 +157,21 @@ class MainPanel extends PureComponent {
                     <div className='Spacer'></div>
                     <a className='CollapseButton' onClick={this.handleToggleExpand}></a>
                   </div>
-                  <div className='CollapsePanelContent'>
-                    <div className='PolicyItem'>
+                  <div className='CollapsePanelContent DropZone'>
+                    <div className='PolicyItem' draggable='true'>
                       <div className='Name'>LMI - Default</div>
                       <div className='Description'>Description...</div>
-                      <div className='Category'>Integrations</div>
+                      <div className='Category Integrations'>Integrations</div>
                     </div>
-                    <div className='PolicyItem'>
+                    <div className='PolicyItem' draggable='true'>
                       <div className='Name'>LMI - GP</div>
                       <div className='Description'>Description...</div>
-                      <div className='Category'>Integrations</div>
+                      <div className='Category Integrations'>Integrations</div>
                     </div>
-                    <div className='PolicyItem'>
+                    <div className='PolicyItem' draggable='true'>
                       <div className='Name'>Webroot</div>
                       <div className='Description'>Description...</div>
-                      <div className='Category'>Integrations</div>
+                      <div className='Category Integrations'>Integrations</div>
                     </div>
                   </div>
                 </div>
@@ -137,16 +185,16 @@ class MainPanel extends PureComponent {
                     <div className='Spacer'></div>
                     <a className='CollapseButton' onClick={this.handleToggleExpand}></a>
                   </div>
-                  <div className='CollapsePanelContent'>
-                    <div className='PolicyItem'>
+                  <div className='CollapsePanelContent DropZone'>
+                    <div className='PolicyItem' draggable='true'>
                       <div className='Name'>Policy 01</div>
                       <div className='Description'>Description...</div>
-                      <div className='Category'>Network</div>
+                      <div className='Category Network'>Network</div>
                     </div>
-                    <div className='PolicyItem'>
+                    <div className='PolicyItem' draggable='true'>
                       <div className='Name'>Policy 02</div>
                       <div className='Description'>Description...</div>
-                      <div className='Category'>Network</div>
+                      <div className='Category Network'>Network</div>
                     </div>
                   </div>
                 </div>
@@ -160,16 +208,16 @@ class MainPanel extends PureComponent {
                     <div className='Spacer'></div>
                     <a className='CollapseButton' onClick={this.handleToggleExpand}></a>
                   </div>
-                  <div className='CollapsePanelContent'>
-                    <div className='PolicyItem'>
+                  <div className='CollapsePanelContent DropZone'>
+                    <div className='PolicyItem' draggable='true'>
                       <div className='Name'>Agreement GP</div>
                       <div className='Description'>Description...</div>
-                      <div className='Category'>Finance</div>
+                      <div className='Category Finance'>Finance</div>
                     </div>
-                    <div className='PolicyItem'>
+                    <div className='PolicyItem' draggable='true'>
                       <div className='Name'>Agreement RP</div>
                       <div className='Description'>Description...</div>
-                      <div className='Category'>Finance</div>
+                      <div className='Category Finance'>Finance</div>
                     </div>
                   </div>
                 </div>
@@ -184,21 +232,21 @@ class MainPanel extends PureComponent {
                     <div className='Spacer'></div>
                     <a className='CollapseButton' onClick={this.handleToggleExpand}></a>
                   </div>
-                  <div className='CollapsePanelContent'>
-                    <div className='PolicyItem'>
+                  <div className='CollapsePanelContent DropZone'>
+                    <div className='PolicyItem' draggable='true'>
                       <div className='Name'>Policy 03</div>
                       <div className='Description'>Description...</div>
-                      <div className='Category'>Devices</div>
+                      <div className='Category Devices'>Devices</div>
                     </div>
-                    <div className='PolicyItem'>
+                    <div className='PolicyItem' draggable='true'>
                       <div className='Name'>Policy 04</div>
                       <div className='Description'>Description...</div>
-                      <div className='Category'>Devices</div>
+                      <div className='Category Devices'>Devices</div>
                     </div>
-                    <div className='PolicyItem'>
+                    <div className='PolicyItem' draggable='true'>
                       <div className='Name'>Policy 05</div>
                       <div className='Description'>Description...</div>
-                      <div className='Category'>Devices</div>
+                      <div className='Category Devices'>Devices</div>
                     </div>
                   </div>
                 </div>
@@ -212,21 +260,21 @@ class MainPanel extends PureComponent {
                     <div className='Spacer'></div>
                     <a className='CollapseButton' onClick={this.handleToggleExpand}></a>
                   </div>
-                  <div className='CollapsePanelContent'>
-                    <div className='PolicyItem'>
+                  <div className='CollapsePanelContent DropZone'>
+                    <div className='PolicyItem' draggable='true'>
                       <div className='Name'>Policy 06</div>
                       <div className='Description'>Description...</div>
-                      <div className='Category'>Notifications</div>
+                      <div className='Category Notifications'>Notifications</div>
                     </div>
-                    <div className='PolicyItem'>
+                    <div className='PolicyItem' draggable='true'>
                       <div className='Name'>Policy 07</div>
                       <div className='Description'>Description...</div>
-                      <div className='Category'>Notifications</div>
+                      <div className='Category Notifications'>Notifications</div>
                     </div>
-                    <div className='PolicyItem'>
+                    <div className='PolicyItem' draggable='true'>
                       <div className='Name'>Policy 08</div>
                       <div className='Description'>Description...</div>
-                      <div className='Category'>Notifications</div>
+                      <div className='Category Notifications'>Notifications</div>
                     </div>
                   </div>
                 </div>
@@ -240,21 +288,21 @@ class MainPanel extends PureComponent {
                     <div className='Spacer'></div>
                     <a className='CollapseButton' onClick={this.handleToggleExpand}></a>
                   </div>
-                  <div className='CollapsePanelContent'>
-                    <div className='PolicyItem'>
+                  <div className='CollapsePanelContent DropZone'>
+                    <div className='PolicyItem' draggable='true'>
                       <div className='Name'>Policy 09</div>
                       <div className='Description'>Description...</div>
-                      <div className='Category'>Project</div>
+                      <div className='Category Project'>Project</div>
                     </div>
-                    <div className='PolicyItem'>
+                    <div className='PolicyItem' draggable='true'>
                       <div className='Name'>Policy 10</div>
                       <div className='Description'>Description...</div>
-                      <div className='Category'>Project</div>
+                      <div className='Category Project'>Project</div>
                     </div>
-                    <div className='PolicyItem'>
+                    <div className='PolicyItem' draggable='true'>
                       <div className='Name'>Policy 11</div>
                       <div className='Description'>Description...</div>
-                      <div className='Category'>Project</div>
+                      <div className='Category Project'>Project</div>
                     </div>
                   </div>
                 </div>
